4b7b99da7c96b9738bd09b0dfaa2648367d1211f
marimo123-sudo <galievr323@gmail.com>
2024-07-31 23:31:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+  // Проверяем, какая тема у пользователя и применяем соответствующие стили
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add('dark-mode');
+}
+
+  // Обработчик изменения темы
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
+    const newColorScheme = e.matches ? "dark" : "light";
+    document.body.classList.toggle('dark-mode', e.matches);
+});
+
+if (window.devicePixelRatio !== 1) { // Костыль для определения иных устройств, с коэффициентом отличным от 1		
+  var dpt = window.devicePixelRatio;
+  var widthM = window.screen.width * dpt;
+  var widthH = window.screen.height * dpt;
+  document.write('<meta name="viewport" content="width=' + widthM+ ', height=' + widthH + '">');
+}
+
